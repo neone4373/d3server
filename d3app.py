58293681e31c27fe7430bb8b155479c9adf510b9
Neo Ellison <nehemiah.ellison@gmail.com>
2013-02-21 11:22:32 -0500

    Added some analytics!

--- d3app.py
+++ d3app.py
@@ -3,12 +3,19 @@
     This little bit of code powers my d3Sever!!!
 
 """
-import os
+import os, analytics
 from flask import Flask, jsonify, render_template, request, abort
 from jinja2 import TemplateNotFound
 
+analytics.init('82cl6i9bo6h4cnptal9b')
+
 #instatntiate the web app
 app = Flask(__name__)
+
+#determines if I am using this on Titan and turns of analytics
+@app.context_processor
+def isItTitan():
+  return dict(Titan = os.environ['COMPUTERNAME'] == 'TITAN')
 
 #define the rout for the index page
 @app.route('/')
